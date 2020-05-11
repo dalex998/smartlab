1816218a419ad891b39006389f2eba15df320099
Alexandru <dumaalex998@hotmail.it>
2020-05-11 21:40:34 +0200

    Add environment variables

--- app.js
+++ app.js
@@ -7,12 +7,7 @@ const userRouter = require("./routes/userRoutes");
 const app = express();
 
 app.use(express.json());
-app.use(morgan("dev"));
-
-app.use((req, res, next) => {
-  console.log("Hello from the middleware");
-  next();
-});
+if (process.env.NODE_ENV === "development") app.use(morgan("dev"));
 
 app.use("/api/v1/devices", deviceRouter);
 app.use("/api/v1/users", userRouter);
