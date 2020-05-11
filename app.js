ba64b9948d781bc3a946eea58f606af958e4b615
Alexandru <dumaalex998@hotmail.it>
2020-05-11 20:10:04 +0200

    better file structure

--- app.js
+++ app.js
@@ -17,7 +17,4 @@ app.use((req, res, next) => {
 app.use("/api/v1/devices", deviceRouter);
 app.use("/api/v1/users", userRouter);
 
-const port = 3000;
-app.listen(port, () => {
-  console.log(`App running on port ${port}...`);
-});
+module.exports = app;
