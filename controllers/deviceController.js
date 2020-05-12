f5b1ef09c08c2e48bdeac204382bb8422f8ed280
Alexandru <dumaalex998@hotmail.it>
2020-05-12 21:32:23 +0200

    CRUD with mongoose

--- controllers/deviceController.js
+++ controllers/deviceController.js
@@ -1,40 +1,70 @@
 const fs = require("fs");
 const Device = require("./../models/deviceModel");
 
-exports.getAllDevices = (req, res) => {
+exports.getAllDevices = async (req, res) => {
+  const devices = await Device.find();
   res.status(200).send({
     status: "success",
-    length: devices.length,
-    data: { devices },
-  });
-};
-
-exports.getDevice = (req, res) => {
-  res.status(200).send({
-    status: "success",
-    data: { device },
-  });
-};
-
-exports.updateDevice = (req, res) => {
-  res.status(200).json({
-    status: "success",
+    results: devices.length,
     data: {
-      device: "Updated device here...",
+      devices,
     },
   });
 };
 
-exports.deleteDevice = (req, res) => {
+exports.getDevice = async (req, res) => {
+  const { id } = req.params;
+  const device = await Device.findById(id);
+  res.status(200).send({
+    status: "success",
+    data: {
+      device,
+    },
+  });
+};
+
+exports.updateDevice = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    //the updated document will be returned to the client with new: true
+    const device = await Device.findByIdAndUpdate(id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    res.status(200).json({
+      status: "success",
+      data: {
+        device,
+      },
+    });
+  } catch (err) {
+    res.send(err);
+  }
+};
+
+exports.deleteDevice = async (req, res) => {
+  const { id } = req.params;
+  await Device.findByIdAndDelete(id);
   res.status(204).json({
     status: "success",
     data: null,
   });
 };
 
-exports.createDevice = (req, res) => {
-  res.status(201).send({
-    status: "success",
-    data: { newdevice },
-  });
+exports.createDevice = async (req, res) => {
+  try {
+    const newDevice = await Device.create(req.body);
+    res.status(201).send({
+      status: "success",
+      data: {
+        device: newDevice,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      message: err,
+    });
+  }
 };
